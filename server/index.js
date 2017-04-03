eea211e64ffa719979e8b863b11503ef9e2c1319
Swati Mukherjee <swati@cronj.com>
2017-04-03 16:18:05 +0530

    Testing merge

--- server/index.js
+++ server/index.js
@@ -4,4 +4,4 @@ let env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 // Export the application
 exports = module.exports = require('./app');
 
-//test comment
+//Hello i am testing merge

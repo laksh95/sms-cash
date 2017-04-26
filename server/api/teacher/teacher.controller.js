84344d65b04fa17f1f54da4fcf59dd938c08f509
Ankit <ankit@cronj.com>
2017-04-26 18:01:28 +0530

    Teacher backend add teacher and send email

--- server/api/teacher/teacher.controller.js
+++ server/api/teacher/teacher.controller.js
@@ -20,7 +20,7 @@ let teacherFunctions={
         })
       }
       else{
-  			res.status(400).json({error: "Missing Paramters: courseId", message: 'IS_INVALID_INPUT_FORM'})
+  			res.status(400).json({error: "Missing Paramters", message: 'IS_INVALID_INPUT_FORM'})
   		}
   },
   deleteTeacher: (req, res) => {
@@ -34,7 +34,7 @@ let teacherFunctions={
       })
     }
     else{
-      res.status(400).json({error: "Missing Paramters: courseId", message: 'IS_INVALID_INPUT_FORM'})
+      res.status(400).json({error: "Missing Paramters", message: 'IS_INVALID_INPUT_FORM'})
     }
   },
   approveDetails: (req, res) => {
@@ -48,22 +48,46 @@ let teacherFunctions={
       })
     }
     else{
-      res.status(400).json({error: "Missing Paramters: courseId", message: 'IS_INVALID_INPUT_FORM'})
+      res.status(400).json({error: "Missing Paramters", message: 'IS_INVALID_INPUT_FORM'})
     }
   },
   changeDetails: (req, res) => {
     if(req !== null && req != undefined && req.body != undefined && Object.keys(req).length!==0 && Object.keys(req.body).length!==0 || req.user != null){
       teacher.changeDetails(db, req.body)
       .then((result)=>{
+        
           res.status(200).json({result: req.body, suc: result, message: 'SUCCESS_OPERATION'})
       })
       .catch((err)=>{
+     
         res.status(500).json({error: err.toString(), teacher: req.body.teacherId, message: 'IS_INTERNAL_SERVER_ERROR'})
       })
     }
     else{
-      res.status(400).json({error: "Missing Paramters: courseId", message: 'IS_INVALID_INPUT_FORM'})
+      res.status(400).json({error: "Missing Paramters", message: 'IS_INVALID_INPUT_FORM'})
     }
+  },
+  addTeacher:(req , res)=>{
+
+     if(req !== null && req != undefined && req.body != undefined && Object.keys(req).length!==0 && Object.keys(req.body).length!==0 || req.user != null){
+      teacher.addTeacher(db, req.body)
+      .then((result)=>{
+        console.log("Success");
+          res.status(200).json({result: req.body, suc: result, message: 'SUCCESS_OPERATION'})
+      })
+      .catch((err)=>{
+        
+         console.log("Invalid");
+        res.status(500).json({error: err.toString(), teacher: req.body.teacherId, message: 'IS_INTERNAL_SERVER_ERROR'})
+      })
+    }
+    else{
+         console.log("Invalid");
+      res.status(400).json({error: "Missing Paramters", message: 'IS_INVALID_INPUT_FORM'})
+    }
+
+
+
   },
   getTeacherAndFeedback: (req, res) => {
     if(req !== null && req != undefined && req.body != undefined && Object.keys(req).length!==0 && Object.keys(req.body).length!==0 || req.user != null){

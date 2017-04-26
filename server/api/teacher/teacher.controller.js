b76240c9be5ffb5862827f2c79cbc2eea08e3d13
Shilpa Chourasia <shilpa@cronj.com>
2017-04-26 12:06:10 +0530

    console.log removed

--- server/api/teacher/teacher.controller.js
+++ server/api/teacher/teacher.controller.js
@@ -55,7 +55,6 @@ let teacherFunctions={
     if(req !== null && req != undefined && req.body != undefined && Object.keys(req).length!==0 && Object.keys(req.body).length!==0 || req.user != null){
       teacher.changeDetails(db, req.body)
       .then((result)=>{
-        promise.reject()
           res.status(200).json({result: req.body, suc: result, message: 'SUCCESS_OPERATION'})
       })
       .catch((err)=>{

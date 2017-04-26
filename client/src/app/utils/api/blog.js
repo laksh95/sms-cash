41e46ce04e99f873287f43372659558bd1b9ec5d
Yash Sharma <95yashsharma@gmail.com>
2017-04-26 13:26:46 +0530

    / added in api calls

--- client/src/app/utils/api/blog.js
+++ client/src/app/utils/api/blog.js
@@ -12,65 +12,59 @@ let configHeader= ()=> {
 }
 const utils ={
     getPosts : (data) =>{
-        let url = serverAddress + 'api/post/getPosts';
+        let url = serverAddress + '/api/post/getPosts';
         let config = configHeader();
         return axios.post(url,data,  config);
     },
     getPost : (data) =>{
-        let url = serverAddress + 'api/post/getPost';
+        let url = serverAddress + '/api/post/getPost';
         let config = configHeader();
         return axios.post(url,data,  config);
     },
     addComment : (data) =>{
-        let url = serverAddress + 'api/post/addComment';
+        let url = serverAddress + '/api/post/addComment';
         let config= configHeader();
         return axios.post(url,data,  config);
     },
     editComment : (data) =>{
-        let url = serverAddress + 'api/post/editComment';
+        let url = serverAddress + '/api/post/editComment';
         let config= configHeader();
         return axios.post(url,data,  config);
     },
     deleteComment : (data)=>{
-        let url = serverAddress + 'api/post/deleteComment';
+        let url = serverAddress + '/api/post/deleteComment';
         let config= configHeader();
         return axios.post(url,data,  config);
     },
     setLikes : (data)=>{
-        let url = serverAddress + 'api/post/setLikes';
+        let url = serverAddress + '/api/post/setLikes';
         let config= configHeader();
         return axios.post(url,data,  config);
     },
     addPost : (data)=>{
-        let url = serverAddress + 'api/post/addPost';
+        let url = serverAddress + '/api/post/addPost';
         let config= configHeader();
-        // let formData = new FormData()
-        // formData.append('file',data.image)
-        // formData.append('name',data.image.name)
-        // formData.append('type',data.image.type)
-        // formData.append('heading',data.heading)
-        // formData.append('content',data.content)
         return axios.post(url,data,config);
     },
     getStats :(data)=>{
-        let url = serverAddress + 'api/post/getStats';
+        let url = serverAddress + '/api/post/getStats';
         let config= configHeader();
         console.log("inside util",data)
         return axios.post(url,data,config)
     },
     deletePost :(data)=>{
-        let url = serverAddress + 'api/post/deletePost';
+        let url = serverAddress + '/api/post/deletePost';
         let config= configHeader();
         return axios.post(url,data,config)
 
     },
     searchPost : (data)=>{
-        let url = serverAddress + 'api/post/searchPost';
+        let url = serverAddress + '/api/post/searchPost';
         let config= configHeader();
         return axios.post(url,data,config)
     },
     getComments:(data)=>{
-        let url = serverAddress + 'api/post/getComments'
+        let url = serverAddress + '/api/post/getComments'
         let config = configHeader()
         return axios.post(url,data,config)
     }

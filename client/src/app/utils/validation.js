0554b636903b528b2d0e90e667d621f53832a32d
laksh95 <goel.laksh95@gmail.com>
2017-04-27 10:06:10 +0530

    student functionality updated, edit dialog working

--- client/src/app/utils/validation.js
+++ client/src/app/utils/validation.js
@@ -18,3 +18,29 @@ export function isLengthInvalid(value, min, max){
 	return false;
 }
 
+export function isName(name){
+    console.log('validation called')
+     let specialCharacter = name.match(/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/g)
+     let number = name.match(/\d+/g)
+     if(specialCharacter!==null || number !==null){
+         return false
+     }
+     else
+         return true
+
+}
+export function isContactNumber(number){
+    let num = number.match(/\d+/g)
+    if(num === null)
+        return false
+    else
+        return true
+}
+export function isEmail(email){
+    let mail = email.match(/\S+@\S+\.\S+/g)
+    if(mail === null){
+        return false
+    }
+    else return true
+
+}

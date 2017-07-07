68b22201d0fb1474bec7945ad4e1b644102b398b
laksh95 <goel.laksh95@gmail.com>
2017-07-07 21:26:55 +0530

    update github

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 // Set default node environment to development
-let env = process.env.NODE_ENV = process.env.NODE_ENV || 'test';
+let env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 console.log(env)
 // Export the application
 exports = module.exports = require('./app');
